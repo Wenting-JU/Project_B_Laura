dd338dc7de7406fdfd2445fe728ed28331a99f35
char-duncan <84029100+char-duncan@users.noreply.github.com>
2021-05-20 11:07:56 -0400

    Update tut_script.js
    Updated with functions to go to the new documentation pages

--- tut_script.js
+++ tut_script.js
@@ -1,7 +1,7 @@
 //
-// Tutorial JS File.
-// 2021-05-11
-// 
+// JS File for DOCHUB
+// 2021-05-20
+// Charlotte Duncan and Laura Ju
 
 
 // void function; no return.
@@ -14,33 +14,6 @@ function init()
 init();
 
 
-// void function; no return. Inserts the "GEF;" text in the appropriate tag-section.
- 
-function buttonAction()
-{
-	var str = document.getElementById("second_p").innerHTML;
-	document.getElementById("second_p").innerHTML = str + "<b>GEF; </b>";
-
-	// if the content in the tag with id "second_p" has length >80, send a JS-alert.
-	if(str.length > 80)
-	{
-		alert("There's a lot og 'GEF;' tags already.\n\nNOTE: after clicking 'OK', another tag will be added.");
-	}
-}
-
-// void function; no return. Opens the secondary page in the existing window.
-function Page1()
-{
-	window.open("page1.html", "_self");
-}
-function Page2()
-{
-	window.open("page2.html", "_self");
-}
-function Page3()
-{
-	window.open("page3.html", "_self");
-}
 // void function; no return. Opens the main page in the existing window.
 function homePage()
 {
@@ -65,6 +38,14 @@ function newDocs(){
 function newPDoc(){
 	window.open("newPDoc.html", "_self");
 }
+function newCDoc(){
+	window.open("newCDoc.html", "_self");
+}
+function newUDoc(){
+	window.open("newUDoc.html", "_self");
+}
+function save(){
+	window.open("newDocs.html", "_self");
 function searchFunction(){
 	
 	//declare variables
